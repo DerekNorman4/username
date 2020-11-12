5d14ad644b16ef485d8ca6e351cc9e67e74e85e1
Derek Norman <dereknorman42@gmail.com>
2020-11-12 15:32:17 -0800

    updated code for username.sh

--- username.sh
+++ username.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
-# zip.sh
-# author
-echo "Enter a five-digit ZIP code: "
-read ZIP
-while echo "$ZIP" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+# username.sh
+# Derek Norman
+echo "Use lower case characters, digits and underscore"
+echo "It must start with a lower case character"
+echo "Please enter a username: "
+read USERNAME
+
+while echo $USERNAME | egrep -v "^[a-z][a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+	echo "Please enter a valid username"
+	echo "Use lower case characters, digits and underscore"
+	echo "3 - 12 characters long"
+	echo "Enter username: "
+	read USERNAME
 done
 echo "Thank you"
